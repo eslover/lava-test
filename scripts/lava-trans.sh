b4e52fe66016bada83310c60d5b41f8702afb95b
Jason Liu <r64343@freescale.com>
2014-12-23 17:45:42 +0800

    update
    Signed-off-by: Jason Liu <r64343@freescale.com>

---
+++ scripts/lava-trans.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "please specify source file!"
+	exit 1
+fi
+
+if [ -z $2 ]
+then
+	echo "please specify target file!" 
+	exit 2
+fi
+
+if [ -f $2 ]
+then
+	echo file $2 is existing, will try to remove it?
+	rm -i $2
+fi
+
+echo "try to transform $1 to $2 with lava yaml format"
+
+while read CMD; do
+	x=`echo "$CMD" | cut -d ' ' -f 1`
+	y=`echo "$CMD" | cut -d ' ' -f 2-`
+	z=`echo $y | rev`
+	p=`echo $z | cut -c 2- | rev`
+	echo '       ' - lava-test-case ${x}  --shell ./scripts/lava-vte.sh \'${p}\' >> $2 
+done < "$1"
+
