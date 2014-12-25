25208c973d8b0319e78154f643f0cab6d6da072a
Jason Liu <r64343@freescale.com>
2014-12-25 13:19:18 +0800

    update
    Signed-off-by: Jason Liu <r64343@freescale.com>

--- scripts/mm_auto/mm_test.sh
+++ scripts/mm_auto/mm_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo `pwd`
 mkdir /mnt/auto_test
 mount -o nolock,tcp -t nfs 10.192.224.45:/streams/ttVector/SHAVectors /mnt/auto_test
-echo `pwd`
 ./auto_test.sh -d /mnt/auto_test -AV -e gst1.0_not_support.txt < CMD
