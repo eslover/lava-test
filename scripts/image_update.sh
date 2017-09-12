bfdb89c0fa6b8d9425f0b8f376819481d0e1301c
Jason Liu <r64343@freescale.com>
2017-09-12 18:24:23 +0800

    debug
    Signed-off-by: Jason Liu <r64343@freescale.com>

--- scripts/image_update.sh
+++ scripts/image_update.sh
@@ -37,29 +37,27 @@ cd ./wget-1.19
             --sysconfdir=/etc  \
             --with-ssl=openssl && make -s && make install
 
-# clean up local boot image
-#rm -f ${bootimage} 
-# get the boot image
-wget -N --backups=1 http://yb2.am.freescale.net/build-output/Linux_IMX_4.9_morty_trunk_next/latest/common_bsp/imx-boot/${bootimage}
-
 # create mount point
 sudo mkdir /rootfs
 # mount the server nfsroot
 mount -t nfs -o nolock 10.192.242.241:/nfsroot/${mount} /rootfs
 cd /rootfs
-# clean up the local rootfs.tar.bz2
-#rm -rf ${nfsroot}.tar.bz2
-#rm -f *{nfsroot} *.tar.bz2
-wget -N --backups=1 -r -l1 -nH --cut-dirs=2 --no-parent -A *{nfsroot}*.tar.bz2 --no-directories \
+
+# get the boot image
+wget -N -q --backups=1 http://yb2.am.freescale.net/build-output/Linux_IMX_4.9_morty_trunk_next/latest/common_bsp/imx-boot/${bootimage}
+wget -N -q --backups=1 -r -l1 -nH --cut-dirs=2 --no-parent -A *{nfsroot}*.tar.bz2 --no-directories \
 	 http://shlinux22.ap.freescale.net/internal-only/Linux_IMX_4.9_morty_trunk_next/latest/fsl-imx-internal-xwayland/
+
+# update the image, rootfs, flash.bin
 
 for i in *{nfsroot}*.tar.bz2; do
   if [ -f "$i" ]; then ln -sf $i ${nfsroot}.tar.bz2; fi
 done
 
-sudo umount /rootfs
-
 # program the boot image to the sd-card
 sudo dd if=${bootimage} of=/dev/${mmcblock} bs=33k seek=1;sync
+
+cd ~
+sudo umount /rootfs
 
 echo "update the image.... done"
