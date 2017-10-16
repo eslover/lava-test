8b9cff2d31cd73feeebe5ec44a57ba97c93c2115
Jason Liu <r64343@freescale.com>
2017-10-15 23:00:04 -0400

    swtich to beta release test
    Signed-off-by: Jason Liu <r64343@freescale.com>

--- scripts/auto_run.sh
+++ scripts/auto_run.sh
@@ -22,32 +22,39 @@ N_plt=${#plt[@]}
 
 while [ 1 ]; do
 
-cd ${wd}
+	cd ${wd}
 
-wget -q --backups=1 http://shlinux22.ap.freescale.net/internal-only/Linux_IMX_4.9_morty_trunk_next/latest/fsl-imx-internal-xwayland/test-internal-qt5-imx8qxpmek-license.manifest
+	wget -q --backups=1 http://shlinux22.ap.freescale.net/internal-only/Linux_IMX_MX8_BETA/latest/fsl-imx-internal-xwayland/test-internal-qt5-imx8qxpmek-license.manifest
 
-cmp test-internal-qt5-imx8qxpmek-license.manifest test-internal-qt5-imx8qxpmek-license.manifest.1
+	if [ $? -eq 0 ]
+	then
 
-if [ $? -eq 0 ]
-then
-	sleep 10
-else
-	for (( i=1; i<${N_plt}+1; i++ )); do
-		cd ${wd}
-		echo "going to delopy platform: ${plt[$i-1]}"
-		mkdir -p ${plt[$i-1]}
-		cd ${plt[$i-1]}
-		rm -rf *.tar.bz2
+		cmp test-internal-qt5-imx8qxpmek-license.manifest test-internal-qt5-imx8qxpmek-license.manifest.1
 
-		wget -N -q --backups=1 -r -l1 -nH --cut-dirs=2 --no-parent -A "*${nfs[$i-1]}*.tar.bz2" --no-directories \
-			 http://shlinux22.ap.freescale.net/internal-only/Linux_IMX_4.9_morty_trunk_next/latest/fsl-imx-internal-xwayland/
+		if [ $? -eq 0 ]
+		then
+			sleep 60
+		else
+			for (( i=1; i<${N_plt}+1; i++ )); do
+				cd ${wd}
+				echo "going to delopy platform: ${plt[$i-1]}"
+				mkdir -p ${plt[$i-1]}
+				cd ${plt[$i-1]}
+				rm -rf *.tar.bz2
 
-		ln -sf *${nfs[$i-1]}*.tar.bz2 ${nfs[$i-1]}.tar.bz2;
+				wget -N -q --backups=1 -r -l1 -nH --cut-dirs=2 --no-parent -A "*${nfs[$i-1]}*.tar.bz2" --no-directories \
+					http://shlinux22.ap.freescale.net/internal-only/Linux_IMX_MX8_BETA/latest/fsl-imx-internal-xwayland/
 
-		#start the job
-		/home/r64343/workspace/lava-test/test/${plt[$i-1]}/start_ci.sh
-	done
+				ln -sf *${nfs[$i-1]}*.tar.bz2 ${nfs[$i-1]}.tar.bz2;
 
-fi
+				#start the job
+				/home/r64343/workspace/lava-test/test/${plt[$i-1]}/start_ci.sh
+			done
+
+		fi
+	else
+		sleep 60
+
+	fi
 
 done
