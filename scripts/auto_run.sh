b90651102ad3a96c9f0b5cccd3d2bc6b7997fcde
Jason Liu <r64343@freescale.com>
2020-05-12 11:42:32 +0800

    update scripts
    Signed-off-by: Jason Liu <r64343@freescale.com>

--- scripts/auto_run.sh
+++ scripts/auto_run.sh
@@ -29,7 +29,7 @@ nohup bash -x ./auto_run_base.sh -s=imx7ulp -b=evk -u=uTee-7ulp -i=zImage-imx7ul
 
 nohup bash -x ./auto_run_base.sh -s=imx8mm -b=evk -i=Image-imx8mmevk.bin -n=imx8mmevk > /nfsroot/nohup_imx8mmevk.out 2>&1 &
 
-nohup bash -x ./auto_run_base.sh -s=imx8qxp -b=mek -i=Image-imx8qxpmek.bin -n=imx8qxpmek > /nfsroot/nohup_imx8qxpmek.out 2>&1 &
+nohup bash -x ./auto_run_base.sh -s=imx8qxp -b=mek -i=Image-imx8qxpmek.bin -n=imx8qxpc0mek > /nfsroot/nohup_imx8qxpmek.out 2>&1 &
 
 nohup bash -x ./auto_run_base.sh -s=imx8mq -b=evk -i=Image-imx8mqevk.bin -n=imx8mqevk > /nfsroot/nohup_imx8mqevk.out 2>&1 &
 
